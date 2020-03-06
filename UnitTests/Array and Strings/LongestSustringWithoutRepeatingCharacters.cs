9c8e85f27158e0d2d46120263c684dc71e42c352
Yauheni Liohenki <legenkiyes@gmail.com>
2020-03-06 16:09:32 +0100

    Add LongestSubstringWithoutRepeating solution
    Refactor and add new question

---
+++ UnitTests/Array and Strings/LongestSustringWithoutRepeatingCharacters.cs
@@ -0,0 +1,46 @@
+﻿using leetcodeinterviewquestions.Array_and_Strings;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests.Array_and_Strings
+{
+    public class Tests
+    {
+        private LongestSustringWithoutRepeatingCharacters solution;
+        [SetUp]
+        public void Setup()
+        {
+            solution = new LongestSustringWithoutRepeatingCharacters();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var result = solution.LengthOfLongestSubstring("abcabcbb");
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var result = solution.LengthOfLongestSubstring("bbbbb");
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var result = solution.LengthOfLongestSubstring("pwwkew");
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            var result = solution.LengthOfLongestSubstring("tmmzuxt");
+            Assert.AreEqual(5, result);
+        }
+    }
+}
---
+++ leetcodeinterviewquestions/Array and Strings/LongestSustringWithoutRepeatingCharacters.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodeinterviewquestions.Array_and_Strings
+{
+    public class LongestSustringWithoutRepeatingCharacters
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (s.Length == 0)
+                return 0;
+            var length = 1;
+            var encountered = new List<char>();
+            var startIndex = 0;
+            encountered.Add(s[0]);
+            for (var index = 1; index < s.Length; ++index)
+            {
+                if (encountered.Contains(s[index]))
+                {
+                    while (s[startIndex] != s[index])
+                    {
+                        if (encountered.Contains(s[startIndex]))
+                            encountered.Remove(s[startIndex]);
+                        startIndex++;
+                    }
+                    startIndex++;
+                }
+                else {
+                    encountered.Add(s[index]);
+                    if (length < index - startIndex + 1)
+                    {
+                        length = index - startIndex + 1;
+                    }
+                }
+            }
+
+            return length;
+        }
+    }
+}
