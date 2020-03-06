516b2cf2c3e3d9f4c8ac286c9180abfa57cdb9d5
Yauheni Liohenki <legenkiyes@gmail.com>
2020-03-06 18:08:13 +0100

    add solution increasing triplets subsequence
    add solution increasing triplets subsequence

---
+++ UnitTests/Array and Strings/IncreasingTripletSubsequence.cs
@@ -0,0 +1,32 @@
+﻿using leetcodeinterviewquestions.Array_and_Strings;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests.Array_and_Strings
+{
+    public class TestIncreasingTripletSubsequence
+    {
+        private IncreasingTripletSubsequence solution;
+        [SetUp]
+        public void Setup()
+        {
+            solution = new IncreasingTripletSubsequence();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var result = solution.IncreasingTriplet(new int[] { 1, 2, 3, 4, 5 });
+            Assert.AreEqual(true, result);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var result = solution.IncreasingTriplet(new int[] { 5, 4, 3, 2, 1 });
+            Assert.AreEqual(false, result);
+        }
+    }
+}
---
+++ leetcodeinterviewquestions/Array and Strings/IncreasingTripletSubsequence.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodeinterviewquestions.Array_and_Strings
+{
+    public class IncreasingTripletSubsequence
+    {
+        public bool IncreasingTriplet(int[] nums)
+        {
+            if (nums.Length < 3)
+                return false;
+            var maxAfter = new int[nums.Length];
+            var minBefore = new int[nums.Length];
+            var min = nums[0];
+            var max = nums[nums.Length - 1];
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                if (min > nums[i])
+                    min = nums[i];
+                minBefore[i] = min;
+            }
+
+            for (int i = nums.Length - 1; i >= 0; --i)
+            {
+                if (max < nums[i])
+                    max = nums[i];
+                maxAfter[i] = max;
+            }
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                if (minBefore[i] < nums[i] && nums[i] < maxAfter[i])
+                    return true;
+            }
+            return false;
+        }
+    }
+}
