7fa80ebe15dc06b79a0cd5b444a57d74c7527ce9
Yauheni Liohenki <legenkiyes@gmail.com>
2020-03-19 11:25:48 -0500

    Add NumberOfIslands solution
    Add NumberOfIslands solution

---
+++ UnitTests/Trees and Graphs/NumberOfIslands.cs
@@ -0,0 +1,43 @@
+﻿using leetcodeinterviewquestions.Trees_and_Graphs;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTests.Trees_and_Graphs
+{
+    public class NumberOfIslandsTest
+    {
+        private NumberOfIslands solution;
+        [SetUp]
+        public void Setup()
+        {
+            solution = new NumberOfIslands();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var inputStr = @"11110
+11010
+11000
+00000";
+            var input = inputStr.Split(Environment.NewLine).Select(str => str.ToCharArray()).ToArray();
+            var result = solution.NumIslands(input);
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var inputStr = @"11000
+11000
+00100
+00011";
+            var input = inputStr.Split(Environment.NewLine).Select(str => str.ToCharArray()).ToArray();
+            var result = solution.NumIslands(input);
+            Assert.AreEqual(3, result);
+        }
+    }
+}
---
+++ leetcodeinterviewquestions/Trees and Graphs/NumberOfIslands.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodeinterviewquestions.Trees_and_Graphs
+{
+    public class NumberOfIslands
+    {
+        public int NumIslands(char[][] grid)
+        {
+            var copy = new int[grid.Length][];
+            var index = 0;
+            int islands = 0;
+            foreach (var row in grid)
+            {
+                copy[index++] = new int[row.Length];
+            }
+            for (var x = 0; x < grid.Length; ++x)
+            {
+                for (var y = 0; y < grid[0].Length; ++y)
+                {
+                    if (grid[x][y] == '1' && copy[x][y] == 0)
+                    {
+                        MarkIsland(grid, copy, x, y, ++islands);
+                    }
+                }
+            }
+            return islands;
+        }
+        public void MarkIsland(char[][] grid, int[][] copy, int x, int y, int num)
+        {
+            if (x < 0 || y < 0 || x >= grid.Length || y >= grid[0].Length || copy[x][y] != 0 || grid[x][y] != '1')
+                return;
+            copy[x][y] = num;
+            MarkIsland(grid, copy, x - 1, y, num);
+            MarkIsland(grid, copy, x + 1, y, num);
+            MarkIsland(grid, copy, x, y - 1, num);
+            MarkIsland(grid, copy, x, y + 1, num);
+        }
+    }
+}
