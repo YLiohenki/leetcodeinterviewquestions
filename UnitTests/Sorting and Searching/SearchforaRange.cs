612b864542b382853d73d2b338d29a80aa04d7ba
Yauheni Liohenki <legenkiyes@gmail.com>
2020-12-01 05:36:24 -0600

    feat: add one 'sort and search' solution

---
+++ UnitTests/Sorting and Searching/SearchforaRange.cs
@@ -0,0 +1,51 @@
+﻿using leetcodeinterviewquestions.Sorting_and_Searching;
+using NUnit.Framework;
+
+namespace UnitTests.Sorting_and_Searching
+{
+    public class TestSearchforaRange
+    {
+        private SearchforaRange solution;
+        [SetUp]
+        public void Setup()
+        {
+            solution = new SearchforaRange();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var arr = new int[] { 5, 7, 7, 8, 8, 10 };
+            var result = solution.SearchRange(arr, 8);
+            Assert.AreEqual(result[0], 3);
+            Assert.AreEqual(result[1], 4);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var arr = new int[] { 5, 7, 7, 8, 8, 10 };
+            var result = solution.SearchRange(arr, 6);
+            Assert.AreEqual(result[0], -1);
+            Assert.AreEqual(result[1], -1);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var arr = new int[] { };
+            var result = solution.SearchRange(arr, 0);
+            Assert.AreEqual(result[0], -1);
+            Assert.AreEqual(result[1], -1);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            var arr = new int[] { 2, 2 };
+            var result = solution.SearchRange(arr, 1);
+            Assert.AreEqual(result[0], -1);
+            Assert.AreEqual(result[1], -1);
+        }
+    }
+}
---
+++ leetcodeinterviewquestions/Sorting and Searching/SearchforaRange.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodeinterviewquestions.Sorting_and_Searching
+{
+    public class SearchforaRange
+    {
+        public int[] SearchRange(int[] nums, int target)
+        {
+            var start = 0;
+            var end = nums.Length - 1;
+            var middle = (start + end) / 2;
+
+            while (start != end && end < nums.Length && start < nums.Length && end >= 0 && start >= 0)
+            {
+                if (nums[middle] < target)
+                {
+                    start = middle + 1;
+                }
+                else if (nums[middle] > target)
+                {
+                    end = middle - 1;
+                }
+                else
+                {
+                    if (middle - 1 >= 0 && nums[middle - 1] == target)
+                        end = middle - 1;
+                    else
+                        end = middle;
+                }
+                middle = (start + end) / 2;
+
+            }
+            var result = new int[2];
+            result[0] = start;
+
+            start = 0;
+            end = nums.Length - 1;
+            middle = (start + end) / 2;
+
+            while (start != end && end < nums.Length && start < nums.Length && end >= 0 && start >= 0)
+            {
+                if (nums[middle] > target)
+                {
+                    end = middle - 1;
+                }
+                else if (nums[middle] < target)
+                {
+                    start = middle + 1;
+                }
+                else
+                {
+                    if (middle + 1 < nums.Length && nums[middle + 1] == target)
+                        start = middle + 1;
+                    else
+                        start = middle;
+                }
+                middle = (start + end + 1) / 2;
+            }
+
+            result[1] = start;
+
+            if (result[0] >= nums.Length || result[1] >= nums.Length || nums[result[0]] != target || nums[result[1]] != target)
+                return new int[] { -1, -1 };
+            return result;
+        }
+    }
+}
