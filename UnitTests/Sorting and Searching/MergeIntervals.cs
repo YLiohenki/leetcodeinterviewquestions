cc5f88cc395327c1dbfa0c02ff5378d2cd76a297
Yauheni Liohenki <legenkiyes@gmail.com>
2020-12-02 04:49:43 -0600

    feat: add solution for merge intervals

---
+++ UnitTests/Sorting and Searching/MergeIntervals.cs
@@ -0,0 +1,52 @@
+﻿using leetcodeinterviewquestions.Sorting_and_Searching;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace UnitTests.Sorting_and_Searching
+{
+    public class TestMergeIntervals
+    {
+        private MergeIntervals solution;
+        [SetUp]
+        public void Setup()
+        {
+            solution = new MergeIntervals();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var arr = new int[][] { new int[] { 1, 3 }, new int[] { 2, 6 }, new int[] { 8, 10 }, new int[] { 15, 18 } };
+            var result = solution.Merge(arr);
+            var expectedResult = new int[][] { new int[] { 1, 6 }, new int[] { 8, 10 }, new int[] { 15, 18 } };
+            Assert.AreEqual(result.Length, expectedResult.Length);
+            for (var ind = 0; ind > expectedResult.Length; ++ind)
+                Assert.AreEqual(expectedResult[ind][0] == result[ind][0] && expectedResult[ind][1] == result[ind][1], true);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var arr = new int[][] { new int[] { 1, 4 }, new int[] { 0, 0 } };
+            var result = solution.Merge(arr);
+            var expectedResult = new int[][] { new int[] { 0, 0 }, new int[] { 1, 4 }};
+            Assert.AreEqual(result.Length, expectedResult.Length);
+            for (var ind = 0; ind > expectedResult.Length; ++ind)
+                Assert.AreEqual(expectedResult[ind][0] == result[ind][0] && expectedResult[ind][1] == result[ind][1], true);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var arr = new int[][] { new int[] { 2, 3 }, new int[] { 5, 5 }, new int[] { 2, 2 }, new int[] { 3, 4 }, new int[] { 3, 4 } };
+            var result = solution.Merge(arr);
+            var expectedResult = new int[][] { new int[] { 2, 4 }, new int[] { 5, 5 } };
+            Assert.AreEqual(expectedResult.Length, result.Length);
+            for (var ind = 0; ind > expectedResult.Length; ++ind)
+                Assert.AreEqual(expectedResult[ind][0] == result[ind][0] && expectedResult[ind][1] == result[ind][1], true);
+        }
+    }
+}
---
+++ leetcodeinterviewquestions/Sorting and Searching/MergeIntervals.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace leetcodeinterviewquestions.Sorting_and_Searching
+{
+    public class MergeIntervals
+    {
+        public int[][] Merge(int[][] intervals)
+        {
+            var currentIntervals = 0;
+            var sortedStartIntervals = intervals.OrderBy(i => i[0]).Select(i => i[0]).ToArray();
+            var sortedEndIntervals = intervals.OrderBy(i => i[1]).Select(i => i[1]).ToArray();
+            var currentStart = 0;
+            var currentEnd = 0;
+            var result = new List<int[]>();
+            var resultToAdd = new int[2];
+            while (currentStart < sortedStartIntervals.Length)
+            {
+                if (sortedStartIntervals[currentStart] < sortedEndIntervals[currentEnd])
+                {
+                    if (currentIntervals == 0)
+                    {
+                        resultToAdd[0] = sortedStartIntervals[currentStart];
+                    }
+                    currentIntervals++;
+                    currentStart++;
+                }
+                else if (sortedStartIntervals[currentStart] > sortedEndIntervals[currentEnd])
+                {
+                    currentIntervals--;
+                    if (currentIntervals == 0)
+                    {
+                        resultToAdd[1] = sortedEndIntervals[currentEnd];
+                        result.Add(resultToAdd);
+                        resultToAdd = new int[2];
+                    }
+                    currentEnd++;
+                }
+                else
+                {
+                    if (currentIntervals == 0)
+                    {
+                        resultToAdd[0] = sortedStartIntervals[currentStart];
+                        currentStart++;
+                        currentIntervals++;
+                    }
+                    else
+                    {
+                        currentStart++;
+                        currentEnd++;
+                    }
+                }
+            }
+            if (intervals.Length > 0)
+            {
+                resultToAdd[1] = sortedEndIntervals[sortedEndIntervals.Length - 1];
+                result.Add(resultToAdd);
+            }
+            return result.ToArray();
+        }
+    }
+}
