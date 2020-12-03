7c7b67b37921ab90ab90421ebc9ece63831b2d14
Yauheni Liohenki <legenkiyes@gmail.com>
2020-12-03 05:17:24 -0600

    feat: add one more solution for sorting problem

---
+++ UnitTests/Sorting and Searching/SearchinRotatedSortedArray.cs
@@ -0,0 +1,50 @@
+﻿using leetcodeinterviewquestions.Sorting_and_Searching;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests.Sorting_and_Searching
+{
+    public class TestSearchinRotatedSortedArray
+    {
+        private SearchinRotatedSortedArray solution;
+        [SetUp]
+        public void Setup()
+        {
+            solution = new SearchinRotatedSortedArray();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var arr = new int[] { 4, 5, 6, 7, 0, 1, 2 };
+            var result = solution.Search(arr, 0);
+            Assert.AreEqual(4, result);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var arr = new int[] { 4, 5, 6, 7, 0, 1, 2 };
+            var result = solution.Search(arr, 3);
+            Assert.AreEqual(-1, result);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var arr = new int[] { 1 };
+            var result = solution.Search(arr, 0);
+            Assert.AreEqual(-1, result);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            var arr = new int[] { 4, 5, 6, 7, 0, 1, 2};
+            var result = solution.Search(arr, 4);
+            Assert.AreEqual(0, result);
+        }
+    }
+}
---
+++ leetcodeinterviewquestions/Sorting and Searching/SearchinRotatedSortedArray.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodeinterviewquestions.Sorting_and_Searching
+{
+    public class SearchinRotatedSortedArray
+    {
+        public int Search(int[] nums, int target)
+        {
+            var start = 0;
+            var end = nums.Length - 1;
+            var middle = (start + end) / 2;
+            while (end - start > 4)
+            {
+                if (nums[start] <= target && target <= nums[middle])
+                {
+                    end = middle;
+                    middle = (start + end) / 2;
+                }
+                else if (nums[middle] <= target && target <= nums[end])
+                {
+                    start = middle;
+                    middle = (start + end) / 2;
+                }
+                else if (nums[start] > nums[middle])
+                {
+                    end = middle;
+                    middle = (start + end) / 2;
+                }
+                else
+                {
+                    start = middle;
+                    middle = (start + end) / 2;
+                }
+            }
+
+            for (int i = start; i <= end; ++i)
+            {
+                if (nums[i] == target)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
