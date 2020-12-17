7e6cae927e173746d7d70aebb4949665decd3b9c
Yauheni Liohenki <legenkiyes@gmail.com>
2020-12-17 16:50:52 -0600

    feat: add sorted matrix optimal search

---
+++ UnitTests/Sorting and Searching/Searcha2DMatrixII.cs
@@ -0,0 +1,50 @@
+﻿using leetcodeinterviewquestions.Sorting_and_Searching;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests.Sorting_and_Searching
+{
+    public class TestSearcha2DMatrixII
+    {
+        private Searcha2DMatrixII solution;
+        [SetUp]
+        public void Setup()
+        {
+            solution = new Searcha2DMatrixII();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var arr = new int[][] { new int[] { 1, 4, 7, 11, 15 }, new int[] { 2, 5, 8, 12, 19 }, new int[] { 3, 6, 9, 16, 22 }, new int[] { 10, 13, 14, 17, 24 }, new int[] { 18, 21, 23, 26, 30 } };
+            var result = solution.SearchMatrix(arr, 5);
+            Assert.AreEqual(true, result);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var arr = new int[][] { new int[] { 1, 4, 7, 11, 15 }, new int[] { 2, 5, 8, 12, 19 }, new int[] { 3, 6, 9, 16, 22 }, new int[] { 10, 13, 14, 17, 24 }, new int[] { 18, 21, 23, 26, 30 } };
+            var result = solution.SearchMatrix(arr, 20);
+            Assert.AreEqual(false, result);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var arr = new int[][] { new int[] { -5 } };
+            var result = solution.SearchMatrix(arr, -5);
+            Assert.AreEqual(true, result);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            var arr = new int[][] { new int[] { 1, 1 } };
+            var result = solution.SearchMatrix(arr, 2);
+            Assert.AreEqual(false, result);
+        }
+    }
+}
---
+++ leetcodeinterviewquestions/Sorting and Searching/Searcha2DMatrixII.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodeinterviewquestions.Sorting_and_Searching
+{
+    public class Searcha2DMatrixII
+    {
+        public bool SearchMatrix(int[][] matrix, int target)
+        {
+            if (matrix.Length == 0 || matrix[0].Length == 0)
+                return false;
+            var startX = 0;
+            var startY = 0;
+            var endX = matrix.Length - 1;
+            var endY = matrix[0].Length - 1;
+            while (endY - startY >= 0 && endX - startX >= 0)
+            {
+                if (matrix[endX][startY] == target)
+                {
+                    return true;
+                }
+                else if (matrix[endX][startY] < target)
+                {
+                    startY++;
+                }
+                else
+                {
+                    endX--;
+                }
+            }
+            return false;
+        }
+    }
+}
