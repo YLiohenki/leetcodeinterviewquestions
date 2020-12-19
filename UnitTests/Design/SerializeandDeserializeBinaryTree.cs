a5d031e2ba612a44f0b1549b32ff8c4681ee55de
Yauheni Liohenki <legenkiyes@gmail.com>
2020-12-19 12:50:05 -0600

    add Design problem solution for deserialization/serialization of binary tree

---
+++ UnitTests/Design/SerializeandDeserializeBinaryTree.cs
@@ -0,0 +1,49 @@
+﻿using leetcodeinterviewquestions.Design;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static leetcodeinterviewquestions.Design.SerializeandDeserializeBinaryTree;
+
+namespace UnitTests.Design
+{
+    public class TestsSerializeandDeserializeBinaryTree
+    {
+        private SerializeandDeserializeBinaryTree solution;
+        [SetUp]
+        public void Setup()
+        {
+            solution = new SerializeandDeserializeBinaryTree();
+        }
+
+        public bool CompareTrees(TreeNode tree1, TreeNode tree2)
+        {
+            if (tree1 == null || tree2 == null)
+                return tree1 == tree2;
+            return tree1.val == tree2.val && ((tree1.left == null && tree2.left == null) || CompareTrees(tree1.left, tree2.left))
+                && ((tree1.right == null && tree2.right == null) || CompareTrees(tree1.right, tree2.right));
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var tree = solution.deserialize("1,-,-");
+            var result = solution.deserialize(solution.serialize(tree));
+            Assert.AreEqual(true, CompareTrees(result,tree));
+        }
+        [Test]
+        public void Test2()
+        {
+            var tree = solution.deserialize("");
+            var result = solution.deserialize(solution.serialize(tree));
+            Assert.AreEqual(true, CompareTrees(result, tree));
+        }
+        [Test]
+        public void Test3()
+        {
+            var tree = solution.deserialize("1,2,-,-,3,4,-,-,5,-,-");
+            var result = solution.deserialize(solution.serialize(tree));
+            Assert.AreEqual(true, CompareTrees(result, tree));
+        }
+    }
+}
---
+++ leetcodeinterviewquestions/Design/SerializeandDeserializeBinaryTree.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcodeinterviewquestions.Design
+{
+    public class SerializeandDeserializeBinaryTree
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+
+        // Your Codec object will be instantiated and called as such:
+        // Codec ser = new Codec();
+        // Codec deser = new Codec();
+        // TreeNode ans = deser.deserialize(ser.serialize(root));
+        public string serialize(TreeNode root)
+        {
+            if (root == null)
+                return "";
+            return root.val.ToString() + "," + (root.left != null ? serialize(root.left) : "-") + "," + (root.right != null ? serialize(root.right) : "-");
+        }
+        public TreeNode deserialize(string data)
+        {
+            if (data == "")
+                return null;
+            var elems = new Queue<string>(data.Split(","));
+            return deserializeRecurs(elems);
+        }
+
+        public TreeNode deserializeRecurs(Queue<string> elems)
+        {
+            var result = new TreeNode(int.Parse(elems.Dequeue()));
+            var left = elems.Peek();
+            if (left != "-")
+            {
+                result.left = deserializeRecurs(elems);
+            }
+            else
+            {
+                elems.Dequeue();
+            }
+            var right = elems.Peek();
+            if (right != "-")
+            {
+                result.right = deserializeRecurs(elems);
+            }
+            else
+            {
+                elems.Dequeue();
+            }
+            return result;
+        }
+    }
+}
