108963fdc0a4b1905924e4e08f8d0193adc7e818
Yauheni Liohenki <legenkiyes@gmail.com>
2020-12-19 13:13:05 -0600

    Add design O(1) GetRandom structure

---
+++ UnitTests/Design/InsertDeleteGetRandomO1.cs
@@ -0,0 +1,34 @@
+﻿using leetcodeinterviewquestions.Design;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests.Design
+{
+    public class TestRandomizedSet
+    {
+        private RandomizedSet solution;
+        [SetUp]
+        public void Setup()
+        {
+            solution = new RandomizedSet();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var result = solution.Insert(0);
+            Assert.AreEqual(true, result);
+            result = solution.Insert(0);
+            Assert.AreEqual(false, result);
+            result = solution.Insert(1);
+            Assert.AreEqual(true, result);
+            result = solution.Insert(2);
+            Assert.AreEqual(true, result);
+            result = solution.Insert(3);
+            Assert.AreEqual(true, result);
+            var randRes = solution.GetRandom();
+        }
+    }
+}
---
+++ leetcodeinterviewquestions/Design/InsertDeleteGetRandomO1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcodeinterviewquestions.Design
+{
+    public class RandomizedSet
+    {
+        Dictionary<int, int> data;
+        Random rnd;
+        public RandomizedSet()
+        {
+            data = new Dictionary<int, int>();
+            rnd = new Random();
+        }
+        public bool Insert(int val)
+        {
+            if (data.ContainsKey(val))
+                return false;
+            data.Add(val, val);
+            return true;
+        }
+        public bool Remove(int val)
+        {
+            return data.Remove(val);
+        }
+        public int GetRandom()
+        {
+            var keyN = rnd.Next(0, data.Keys.Count);
+            return data.Keys.ElementAt(keyN);
+        }
+    }
+}
