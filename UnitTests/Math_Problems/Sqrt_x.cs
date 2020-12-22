ea4ba485e84efc3a42a48c76cbd8f2b662d94fe8
Yauheni Liohenki <legenkiyes@gmail.com>
2020-12-22 17:42:42 -0600

    add sqrt x problem

---
+++ UnitTests/Math_Problems/Sqrt_x.cs
@@ -0,0 +1,59 @@
+﻿using leetcodeinterviewquestions.Math_Problems;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests.Math_Problems
+{
+    public class TestSqrt_x
+    {
+        private Sqrt_x solution;
+        [SetUp]
+        public void Setup()
+        {
+            solution = new Sqrt_x();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var result = solution.MySqrt(4);
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var result = solution.MySqrt(8);
+            Assert.AreEqual(2, result);
+        }
+        [Test]
+        public void Test3()
+        {
+            var result = solution.MySqrt(1);
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            var result = solution.MySqrt(2147395599);
+            Assert.AreEqual(46339, result);
+        }
+
+        [Test]
+        public void Test5()
+        {
+            var result = solution.MySqrt(2147395600);
+            Assert.AreEqual(46340, result);
+        }
+
+        [Test]
+        public void Test6()
+        {
+            var result = solution.MySqrt(29792276);
+            Assert.AreEqual(5458, result);
+        }
+    }
+}
---
+++ leetcodeinterviewquestions/Math_Problems/Sqrt_x.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodeinterviewquestions.Math_Problems
+{
+    public class Sqrt_x
+    {
+        public int MySqrt(int x)
+        {
+            var prev = (x / 2 + 1) * 2;
+            var middle = x / 2;
+            while (true)
+            {
+                if (middle < 46341 && middle * middle <= x && ((middle + 1) >= 46341 || (middle + 1) * (middle + 1) > x))
+                    return middle;
+                if (middle * middle <= x && middle < 46341)
+                {
+                    middle = middle + Math.Max(1, (prev - middle) / 2);
+                }
+                else {
+                    var m = middle;
+                    middle = middle - Math.Max(1 , (prev - middle) / 2);
+                    prev = m;
+                }
+            }
+        }
+    }
+}
